35b49fce4ff6ca2e285bdbf9c3504a91e552c14e
Paul Solovey <p.v.solovey@ya.ru>
2023-02-15 14:39:56 +0300

    HomeWork Iterators and Generators, solved the tasks with asterisks

--- it_gen/gen.py
+++ it_gen/gen.py
@@ -2,10 +2,17 @@ import types
 
 
 def flat_generator(list_of_lists):
+    my_list = []
+    l = list_of_lists.copy()
+    while l:
+        item = l.pop(-1)
+        if type(item) is list:
+            l.extend(item)
+        else:
+            my_list.append(item)
 
-    for item in list_of_lists:
-        for i in item:
-            yield i
+    for i in my_list[::-1]:
+        yield i
 
 
 
@@ -26,6 +33,25 @@ def test_2():
 
     assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
 
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
 
 if __name__ == '__main__':
-    test_2()
+    test_4()
--- it_gen/iter.py
+++ it_gen/iter.py
@@ -1,25 +1,48 @@
 class FlatIterator:
 
     def __init__(self, list_of_list):
-        self.list_of_list = list_of_list
+        self.list_of_list = list_of_list.copy()
+
 
     def __iter__(self):
-        self.cursor_1 = 0
-        self.cursor_2 = -1
+        self.cursor = -1
+        self.my_list = []
+        self.l = self.list_of_list
         return self
 
     def __next__(self):
-        if self.cursor_1 <= len(self.list_of_list):
-            if self.cursor_2 < len(self.list_of_list[self.cursor_1]) - 1:
-                self.cursor_2 += 1
-                return self.list_of_list[self.cursor_1][self.cursor_2]
-            elif self.cursor_1 < len(self.list_of_list) - 1:
-                self.cursor_2 = 0
-                self.cursor_1 += 1
-                return self.list_of_list[self.cursor_1][self.cursor_2]
-            else:
-                raise StopIteration
 
+        while self.l:
+            item = self.l.pop(-1)
+            if type(item) is list:
+                self.l.extend(item)
+            else:
+                self.my_list.append(item)
+
+        if self.cursor < len(self.my_list) - 1:
+            self.list_of_list = self.my_list[::-1]
+            self.cursor += 1
+            return self.list_of_list[self.cursor]
+        else:
+            raise StopIteration
+
+
+def test_3():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
 
 def test_1():
 
@@ -40,4 +63,4 @@ def test_1():
 
 
 if __name__ == '__main__':
-    test_1()
+    test_3()
