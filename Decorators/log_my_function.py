0634097b19a18dfb54576249ad124d23fc17acaa
Paul Solovey <p.v.solovey@ya.ru>
2023-02-17 15:31:01 +0300

    Revert ":wq"
    This reverts commit 2d89042dc6c2230bf4f2694a315e39a813cf46b9.

--- Decorators/log_my_function.py
+++
@@ -1,9 +0,0 @@
-from main import start_program
-from decorator_with_logger import logger
-
-@logger('log.log')
-def new_start_program():
-    return
-
-if __name__ == '__main__':
-    new_start_program()
